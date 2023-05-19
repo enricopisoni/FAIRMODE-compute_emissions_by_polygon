39ad2b55308790b33aff4c45da3ec2c3c24f20cd
mariomi <mirko.marioni@ext.ec.europa.eu>
2023-05-19 15:11:52 +0200

    _ Add final (csv) merge option as different module. _ Add files compliance module (final users must follow standard names on  pollutant/sectors) _ Update of 2016 NUTS and 2020 FUA shape in the polygons folder _ Update of shell scripts (ToBeChecked with EP)

---
+++ sh/main_geotiff.sh
@@ -0,0 +1,15 @@
+
+#put_here_your_inst_folder
+code_root="/storage/mariomi/Shared_PyProj/FAIRMODE-compute_emissions_by_polygon/"
+app_root="/eos/jeodpp/data/projects/FAIRMODE/"
+
+#put_here_your_bin_from_conda (or activate properly the right conda env)
+py_exec=/storage/mariomi/conda/fm_3_10/bin/python3.10
+#put_here_main_py (leave as is)
+py_code=$code_root/py/main_geotiff.py
+
+#test setting, change only after the first working run, if you need it
+#data_root=$app_root
+
+echo $py_exec $py_code
+$py_exec $py_code $app_root
